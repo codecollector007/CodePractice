92345dae4fdd818b9b9fab617bd7bb443b19a273
codecollector007@gmail.com <rnatarajan@go2uti.com>
2016-10-11 21:16:47 -0700

    Initial commit

---
+++ src/main/java/CheckNumeric.java
@@ -0,0 +1,10 @@
+/**
+ * Created by ramya.natarajan on 10/11/2016.
+ */
+public class CheckNumeric {
+
+    public static void main(String[] args) {
+
+        System.out.println ("hello world");
+    }
+}
