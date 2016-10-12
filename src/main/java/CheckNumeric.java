50d1e31e8e9a45c59558fcde17fc015084c68986
codecollector007@gmail.com <rnatarajan@go2uti.com>
2016-10-11 21:30:25 -0700

    check for numeric

--- src/main/java/CheckNumeric.java
+++ src/main/java/CheckNumeric.java
@@ -1,10 +1,25 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import org.apache.commons.lang3.StringUtils;
 /**
- * Created by ramya.natarajan on 10/11/2016.
+ * Class to check if a String is numeric
  */
 public class CheckNumeric {
 
     public static void main(String[] args) {
 
-        System.out.println ("hello world");
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            String input = br.readLine();
+
+            if (StringUtils.isNumeric(input)) {
+                System.out.println ("it is numeric");
+            } else {
+                System.out.println ("it is not numeric");
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
